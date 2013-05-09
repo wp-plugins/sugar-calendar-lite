b39da5c2c6031302e29f8a804ea9bd835d5aa857
mordauk <mordauk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-09 21:33:26 +0000

    Uploading more lang files
    git-svn-id: https://plugins.svn.wordpress.org/sugar-calendar-lite/trunk@710609 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/sc-ajax.js
+++ assets/js/sc-ajax.js
@@ -1,12 +1,11 @@
 jQuery(document).ready(function($) {
 	$('body').on('submit', '.sc_events_form', function() {
-		
+
 		document.body.style.cursor = 'wait';
-		var calendar = $(this).parent().parent().parent().attr('id');
+		var calendar = $(this).parent().parent().parent().parent().attr('id');
 		var data = $(this).serialize();
 	    $.post(sc_vars.ajaxurl, data, function (response) {
 	   		$('#' + calendar).html(response);
-			scResizeCal();
 	 	}).done(function() {
 	 		document.body.style.cursor = 'default';
 	 	});
